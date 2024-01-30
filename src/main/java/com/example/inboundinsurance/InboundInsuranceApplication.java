f9baaf662b867fcea79c60e0b2b60876d5227107
theingiphwe <129860923+theingiphwe@users.noreply.github.com>
2024-01-30 13:21:46 +0630

    Initial commit

---
+++ src/main/java/com/example/inboundinsurance/InboundInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.example.inboundinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboundInsuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InboundInsuranceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inboundinsurance/InboundInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inboundinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboundInsuranceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
